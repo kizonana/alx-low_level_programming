1de828cd59bff9fe08ca12a18135ab9e340ab6e1
kizonana <kizonana93@gmail.com>
2023-02-24 07:18:30 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,21 +12,21 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && b > c)
 	{
 		largest = c;
 	}
-	else if (b > c)
+	else if (c > b)
 	{
-		largest = b;
+		largest = c;
 	}
 	else
 	{
-		largest = c;
+		largest = b;
 	}
 
 	return (largest);
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,5 +6,8 @@
 void positive_or_negative(int i);
 void print_remaining_days(int month, int day, int year);
 int main(void);
+int convert_day(int month, int day);
+int largest_number(int a, int b, int c);
+void print_times_table(int n);
 
 #endif
