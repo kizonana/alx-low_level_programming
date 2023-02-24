6d10fc363aeffaf080a07872f30f332ddcd8beed
kizonana <kizonana93@gmail.com>
2023-02-24 06:17:45 -0800

    3-convert_day.c

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
+{
+	int a, b, c;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	if
+		("a > c b < c")
+
+	printf("%d is the largest number\n", a);
+
+	return (0);
+}
