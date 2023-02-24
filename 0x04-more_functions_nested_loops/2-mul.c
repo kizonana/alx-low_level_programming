fe3d7b7cee3d305c68f8d52bca8b88a120de05af
kizonana <kizonana93@gmail.com>
2023-02-24 12:25:55 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function that multipliers two intergers
+ * @a: first paramter
+ * @b: second paramter
+ * Return: a times b
+ */
+
+int mul(int a, int b)
+{
+	if (a && b)
+	{
+		return (a * b);
+	}
+}
