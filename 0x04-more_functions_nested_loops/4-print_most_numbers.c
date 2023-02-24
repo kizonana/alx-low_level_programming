0b474a9018040f3ae88c4ad6b3b4fa0f2540a455
kizonana <kizonana93@gmail.com>
2023-02-24 13:54:20 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints numbers
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (; c <= 9; c++)
+	{
+		if (c == 2 || c == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c + '0');
+		}
+		_putchar('\n');
+	}
+}
