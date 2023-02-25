e25479776753cd4ba649e6f6959ad322f92d5e67
kizonana <kizonana93@gmail.com>
2023-02-24 16:45:56 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,27 +12,28 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("FizzBuzz");
+			printf(" Fizz");
 		}
-		else if (i % 3)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Fizz");
+			printf(" Buzz");
 		}
-		else if (i % 5)
+		else if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("Buzz");
+			printf(" FizzBuzz");
 		}
-		else
+		if (i == 1)
 		{
 			printf("%d", i);
 		}
-		if (i != 100)
+		else
 		{
-			printf(" ");
+			printf(" %d", i);
 		}
 	}
-	printf('\n');
+	printf("\n");
+
 	return (0);
 }
