5fc933a9e29b22208b120de6191066fbf54e76be
kizonana <kizonana93@gmail.com>
2023-02-27 13:53:53 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every string character
+ * @str: the string
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
